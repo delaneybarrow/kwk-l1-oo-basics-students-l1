cec59aeeae02ff790173c5cb66329b3c7328a485
delaneybarrow <delaneybarrow19@gmail.com>
2018-06-07 22:36:08 +0000

    automatically backed up by learn

--- nike_shoe.rb
+++ nike_shoe.rb
@@ -1,6 +1,6 @@
 class Shoes
   # need to add attributes for color, laces, base, swoosh
-  def color = (color_base) #gets the color for the base of a shoe
+  def color=(color_base) #gets the color for the base of a shoe
     @this_shoes_color = color_base #create a variable for THIS INSTANCE, assigns color_base to that variable 
   end 
   
